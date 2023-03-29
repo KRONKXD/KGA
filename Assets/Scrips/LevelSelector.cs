0add3a96c1ebe56edb7891d9ae91c16db9d75b62
Ginamax123 <riunis112@gmail.com>
2023-03-29 12:11:15 +0300

    LevelSelectionMenu

---
+++ Assets/Scrips/LevelSelector.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public void PlayLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
--- Assets/Scrips/MainMenu.cs
+++ Assets/Scrips/MainMenu.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayGame(string sceneName)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        SceneManager.LoadScene(sceneName);
     }
 
     public void QuitGame()

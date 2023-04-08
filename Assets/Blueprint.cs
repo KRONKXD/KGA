e2e46cd95b185bdfa7d3e77a72c23b65eb8b47c8
ButterBrodas <lord1butt1butter@gmail.com>
2023-04-08 05:19:53 +0300

    bokstu range visual juos statant
    Kai statai bokštą, rodo  blyną kuris nusako jo range.

--- Assets/Blueprint.cs
+++ Assets/Blueprint.cs
@@ -7,6 +7,8 @@ public class Blueprint : MonoBehaviour
     // Start is called before the first frame update
     public Sprite newSprite;
     private Color tempColor;
+    public GameObject buildingRange;
+    private float towerRange = 0f;
     void Start()
     {
         this.GetComponent<SpriteRenderer>().sprite = newSprite;
@@ -21,16 +23,19 @@ public class Blueprint : MonoBehaviour
         if(BuildManager.buildMode)
         {
             newSprite = BuildManager.instance.GetTowerToBuild().GetComponentInChildren<SpriteRenderer>().sprite;
+            towerRange = BuildManager.instance.GetTowerToBuild().GetComponent<turret>().range;
             this.GetComponent<SpriteRenderer>().sprite = newSprite;
             tempColor.a = 0.5f;
             this.GetComponent<SpriteRenderer>().color = tempColor;
             transform.position = BuildManager.instance.gameObject.transform.position;
+            buildingRange.transform.localScale = new Vector3(towerRange * 2, towerRange * 2, towerRange * 2);
+            buildingRange.GetComponent<SpriteRenderer>().enabled = true;
         }
         else
         {
             tempColor.a = 0;
             this.GetComponent<SpriteRenderer>().color = tempColor;
+            buildingRange.GetComponent<SpriteRenderer>().enabled = false;
         }
     }
-
 }

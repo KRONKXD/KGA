03c448f718d2cd320db595b7e3195d9b2e1bde11
ButterBrodas <lord1butt1butter@gmail.com>
2023-04-20 23:46:37 +0300

    spawner reikaliukai

---
+++ Assets/Scrips/EnemySpawn.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField] private float spawnRate=1f;
+    [SerializeField] private GameObject[] enemyPrefabs;
+    [SerializeField] private bool canSpawn = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator Spawner()
+    {
+        WaitForSeconds wait = new WaitForSeconds(spawnRate);
+        while (canSpawn)
+        {
+            yield return wait;
+            int random = Random.Range(0, enemyPrefabs.Length);
+            GameObject enemyToSpawn = enemyPrefabs[random];
+            Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+        }
+    }
+}

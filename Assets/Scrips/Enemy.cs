0f9d40453a510748505822264e69aeb495b5b191
ButterBrodas <lord1butt1butter@gmail.com>
2023-04-26 13:04:26 +0300

    Necro priesas
    dar be savo sprite

--- Assets/Scrips/Enemy.cs
+++ Assets/Scrips/Enemy.cs
@@ -27,17 +27,26 @@ public class Enemy : MonoBehaviour
     public bool minion = false;
 
     //freeze stuff
-    private bool timerRunning = false;
-    private float timer = 0f;
+    private bool freezeTimerRunning = false;
+    private float freezeTimer = 0f;
     private float oldSpeed = 0f;
     private Color frozenColor = Color.blue;
 
+    //necro stuff
+    public bool necromancer = false;
+    public float reviveCD = 0f;
+    //private bool necroTimerRunning = true;
+    private float necroTimer = 0f;
+    private Color revivedColor = Color.gray;
+    public float reviveExec = 0f;
+    private float reviveTimer = 0f;
     static int enemiesKilled;
+    private bool revived = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(!minion)
+        if(!minion && !revived)
         {
             target = waypoint.points[0];
         }
@@ -56,19 +65,48 @@ public class Enemy : MonoBehaviour
             GetNextWaypoint();
         }
 
-        if (timerRunning)
+        if (freezeTimerRunning)
         {
-            //Debug.Log(timer);
-            timer -= Time.smoothDeltaTime;
-            if (timer >= 0)
+            //Debug.Log(freezeTimer);
+            freezeTimer -= Time.smoothDeltaTime;
+            if (freezeTimer >= 0)
             { 
             }
             else
             {
-                timerRunning = false;
-                speed = oldSpeed;
-                this.GetComponent<SpriteRenderer>().color = Color.white;
-                this.GetComponent<Animator>().speed = 1;
+                freezeTimerRunning = false;
+               // speed = oldSpeed;
+                //this.GetComponent<SpriteRenderer>().color = Color.white;
+               // this.GetComponent<Animator>().speed = 1;
+            }
+        }
+
+        if (necromancer)
+        {
+            //Debug.Log(necroTimer);
+            necroTimer -= Time.smoothDeltaTime;
+            if (necroTimer >= 0 )
+            {
+                
+            }
+            else
+            {
+                if (speed != 0)
+                    oldSpeed = speed;
+                speed = 0;
+                reviveTimer -= Time.smoothDeltaTime;
+                if (reviveTimer >= 0)
+                {
+                    //Debug.Log("baiges laikas, bandom revive");
+                    //necroTimerRunning = false;
+                    
+                }
+                else
+                {
+                    Revive();
+                    speed = oldSpeed;
+                }
+                
             }
         }
     }
@@ -89,8 +127,18 @@ public class Enemy : MonoBehaviour
         soundPlayer.PlayOneShot(deadSound);
         MoneyManager.CurrentMoney += bounty;
         enemiesKilled++;
+        if (!minion && !revived)
+        {
+            GameObject copy = Instantiate(this.gameObject, transform.position, transform.rotation);
+            copy.GetComponent<Enemy>().setWaypoint(target, waypointIndex);
+            copy.GetComponent<Enemy>().waypoint = waypoint;
+            copy.SetActive(false);
+            DeadEnemyManager.addDeadEnemy(copy);
+        }
+            
         Destroy(gameObject);
         HealthManager.diedEnemyNumber++;
+        
         if (splitInto != 0)
         {
             for (int i = 0; i < splitInto; i++)
@@ -105,6 +153,7 @@ public class Enemy : MonoBehaviour
                 misplaceMax.y += 0.3f;
                 Random(ref misplace, misplaceMin, misplaceMax);
                 minion = Instantiate(splitMinion, misplace, transform.rotation);
+                minion.GetComponent<SpriteRenderer>().color = this.GetComponent<SpriteRenderer>().color;
                 minion.GetComponent<Enemy>().setWaypoint(target, waypointIndex);
                 minion.GetComponent<Enemy>().waypoint = waypoint;
             }
@@ -124,7 +173,7 @@ public class Enemy : MonoBehaviour
 
     void GetNextWaypoint()
     {
-        if (waypointIndex >= waypoint.points.Length - 1) 
+        if (waypointIndex >= waypoint.points.Length - 1)
         {
             soundPlayer.PlayOneShot(deadSound);
             Destroy(gameObject);
@@ -149,8 +198,31 @@ public class Enemy : MonoBehaviour
         speed = 0;
         this.GetComponent<SpriteRenderer>().color = frozenColor;
         this.GetComponent<Animator>().speed = 0;
-        timerRunning = true;
-        timer = freezeTime;
+        freezeTimerRunning = true;
+        freezeTimer = freezeTime;
+    }
+
+    private void Revive()
+    {
+        
+        GameObject target = DeadEnemyManager.getDeadEnemy();
+        //Debug.Log(target);
+        if(target != null)
+        {
+            //Instantiate(target, transform.position, transform.rotation);
+            target.GetComponent<Enemy>().maxHealth /= 2;
+            target.GetComponent<Enemy>().health = target.GetComponent<Enemy>().maxHealth;
+            target.GetComponent<SpriteRenderer>().color = revivedColor;
+            target.GetComponent<Enemy>().revived = true;
+            target.GetComponent<Enemy>().bounty /= 10;
+            target.SetActive(true);
+        }
+
+        //necroTimerRunning = true;
+        necroTimer = reviveCD;
+        speed = oldSpeed;
+        reviveTimer = reviveExec;
+
     }
 
     public static int GetNumberOfEnemiesDied()
